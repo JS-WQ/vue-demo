badff77f98db4288fee0638ecee4f4ab7b6b477d
unknown <552410301@qq.com>
2018-06-14 21:58:08 +0800

    完成

--- .history/config/index_20180614032725.js
+++ .history/config/index_20180614032725.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-demo/dist',
+    assetsPublicPath: '/new-vue/dist',
     proxyTable: {},
 
     // Various Dev Server settings
