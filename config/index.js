9c8d635d42ea752a56b11281c04e7db38501e9fd
unknown <552410301@qq.com>
2018-06-14 21:27:42 +0800

    路径更新

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/new-vue/dist',
+    assetsPublicPath: '/vue-demo/dist',
     proxyTable: {},
 
     // Various Dev Server settings
